ea39843d7bbf22a84ac90bcaa0c7f9755109a0bf
Hyesun An <hyesun.an@gmail.com>
2011-10-29 20:30:02 -0400

    working makefile and rpc.c

---
+++ rpc.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "rpc.h"
+
+int rpcInit()
+{
+	return 0;
+}
+int rpcCall(char* name, int* argTypes, void** args)
+{
+	return 0;
+}
+int rpcRegister(char* name, int* argTypes, skeleton f)
+{
+	return 0;
+}
+int rpcExecute()
+{
+	return 0;
+}
+int rpcTerminate()
+{
+	return 0;
+}
